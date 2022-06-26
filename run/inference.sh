202fbfaee7e4dd1e4baa7f649c3b13a9feeb5e67
scofiedluo <scofiedluo@gmail.com>
2022-06-26 17:12:34 +0800

    remove unnecessary track

--- run/inference.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ..
-nohup python inference.py --device 0 --work-dir 'inference_results/' \
-                          --checkpoint 'training_results/train_dev/best_61_model.pt' \
-                          --experiment 'test_dev' --window_size 20 --window_stride 20 > inference.log 2>&1 &
--- run/train.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd ..
-
-nohup python main.py --experiment 'train_dev' --device 2 \
-                     --window_size 20 --window_stride 20 --base-lr 0.00005 > train_dev.log 2>&1 &
